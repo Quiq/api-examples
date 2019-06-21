d718bf371d3d106a4a2841be7006e6a116972b30
Cody Wenzel <cody@goquiq.com>
2019-06-21 09:09:50 -0600

    added a CPM example for OSC #NP

--- oracle_service_cloud/send_notification_contact_cpm.php
+++ oracle_service_cloud/send_notification_contact_cpm.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * CPMObjectEventHandler: sendNotificationIncident
+ * CPMObjectEventHandler: sendNotificationContact
  * Package: RN
- * Objects: Incident
+ * Objects: Contact
  * Actions: Create, Update
  * Version: 1.2
  */
@@ -14,11 +14,9 @@ use \RightNow\Connect\v1_2 as RNCPHP;
 //----------------------------------------------------------------------------
 // Description: Sends a notification out via Quiq
 //----------------------------------------------------------------------------
-class sendNotificationIncident implements RNCPM\ObjectEventHandler {
+class sendNotificationContact implements RNCPM\ObjectEventHandler {
 
-    public static function apply($run_mode, $action, $inc, $n_cycles) {
-        $contactId = $inc->PrimaryContact->ID;
-        $contact = RNCPHP\Contact::fetch($contactId);
+    public static function apply($run_mode, $action, $contact, $n_cycles) {
  
         foreach ($contact->Phones as $phoneStruct) {
             $key = ''; //TODO - replace with your own values
@@ -26,10 +24,10 @@ class sendNotificationIncident implements RNCPM\ObjectEventHandler {
             $tenantBaseUrl = ''; // TODO - replace with your own values    
    
             $contactPhoneNumber = $phoneStruct->Number;
-            $contactPoint = '';
-            $topic = '';
+            $contactPoint = ''; //TODO - replace with your own values
+            $topic = ''; //TODO - replace with your own topic
             $messageText = 'Hello '.$contact->Name->First.' '.$contact->Name->Last.' this is an example outbound notification'; //TODO - replace with your own values
-    
+   
             $precedence = array(array("platform" => "SMS"));
             $messageMap = array("default" => array("text" => $messageText));
     
@@ -55,19 +53,15 @@ class sendNotificationIncident implements RNCPM\ObjectEventHandler {
             $res = json_decode(curl_exec($ch));
     
             curl_close($ch);
-    
         }
-
     }
 }
 
-class sendNotificationIncident_TestHarness implements RNCPM\ObjectEventHandler_TestHarness {
+class sendNotificationContact_TestHarness implements RNCPM\ObjectEventHandler_TestHarness {
 
-    static $inc = null;
+    static $con = null;
 
     public static function setup() {
-        $inc2 = new RNCPHP\Incident;
-        $inc2->Subject = "Quiq test";
         $contact = new RNCPHP\Contact();
         $contact->Phones = new RNCPHP\PhoneArray();
         $contact->Phones[0] = new RNCPHP\Phone();
@@ -77,25 +71,20 @@ class sendNotificationIncident_TestHarness implements RNCPM\ObjectEventHandler_T
 		$contact->Phones[0]->PhoneType=new RNCPHP\NamedIDOptList();
 		$contact->Phones[0]->PhoneType->LookupName = "Home Phone";
 		$contact->Phones[0]->Number = "+00008675309";
-		$contact->save(RNCPHP\RNObject::SuppressAll);
-        $inc2->PrimaryContact = $contact;
 
-        static::$inc = $inc2;
+        static::$con = $contact;
         return;
     }
 
     public static function fetchObject($action, $incect_type) {
-        return(static::$inc);
+        return(static::$con);
     }
 
-    public static function validate($action, $inc) {
+    public static function validate($action, $con) {
         return true;
     }
 
     public static function cleanup() {
         return;
     }
-
 }
-
-
