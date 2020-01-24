5c18c424714f7e2ba73fbd3915182cf245f5f21b
Kyle McIntyre <kyle.mcintyre@centricient.com>
2020-01-24 09:29:16 -0700

    Example of Notify

--- notify.py
+++ notify.py
@@ -7,7 +7,7 @@ from quiq_shared import get_headers
 
 parser = argparse.ArgumentParser(description='Sends a message to the specified phone number')
 parser.add_argument('-c', '--contactpoint', required=True, help='The contact point to send from')
-parser.add_argument('-p', '--platform', required=True, help='The platform to send from. Only SMS is supported currently.')
+parser.add_argument('-t', '--topic', required=True, help='The outbound topic to send on')
 parser.add_argument('-n', '--handle', required=True, help='The customer handle (i.e. phone number +1-XXX-XXX-XXXX) to send to.')
 parser.add_argument('-u', '--identity', required=True, help='The app token identity.')
 parser.add_argument('-s', '--secret', required=True, help='The app token secret.')
@@ -16,17 +16,25 @@ parser.add_argument('-d', '--domain', required=True, help='The Quiq domain. i.e.
 args = parser.parse_args()
 
 payload = {
-    'handle': args.handle,
+    'topic': args.topic,
     'contactPoint': args.contactpoint,
-    'message' : {
-        'text': 'Message text to be sent via SMS'
-    }
+    'notifications': [
+        {
+            'precedence': [],
+            'searchInfo': {
+                'phoneNumber': args.handle
+            },
+            'messageMap': {
+                'default': {'text': 'This is an outbound notification sent through Quiq'},
+            }
+        }
+    ]
 }
 
 print("POSTING payload of: {}".format(payload))
 
 headers = get_headers(args.identity,args.secret)
-res = requests.post('https://{}/api/v1/messaging/platforms/{}/send-notification'.format(args.domain, args.platform), data=json.dumps(payload), headers=headers)
+res = requests.post('https://{}/api/v1/messaging/notify'.format(args.domain, args.platform), data=json.dumps(payload), headers=headers)
 
 print("Status Code: {}".format(res.status_code))
 print("Response: {}".format(res.json()))
